d70f8e1258cd558bcf475201d4bcfb447114c958
Laydemir <voldvakoff@gmail.com>
2022-07-12 17:38:10 +0300

    найти проблему

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 			i.style.left = n.clientX + "px",
 			i.style.top = n.clientY + "px"
 	});
-	let t = document.getElementById("cursor");
+	var t = document.getElementById("cursor");
 	e = document.getElementById("cursor2");
 	i = document.getElementById("cursor3");
 	function n(t) {
@@ -19,11 +19,53 @@
 		e.classList.remove("hover"), i.classList.remove("hover")
 	}
 	s();
-	for (let r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+	for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
 		o(r[a])
 	}
 	function o(t) {
 		t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
 	}
 
-})
+	// About page
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	//Contact page
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	//Travel portfolio page
+	$(".travel").on('click', function () {
+		$("body").addClass("travel-on");
+	});
+	$(".travel-close").on('click', function () {
+		$("body").removeClass("travel-on");
+	});
+
+
+	//Wildlife portfolio page
+	$(".wildlife").on('click', function () {
+		$("body").addClass("wildlife-on");
+	});
+	$(".wildlife-close").on('click', function () {
+		$("body").removeClass("wildlife-on");
+	});
+
+
+	//Nature portfolio page
+	$(".nature").on('click', function () {
+		$("body").addClass("nature-on");
+	});
+	$(".nature-close").on('click', function () {
+		$("body").removeClass("nature-on");
+	});
+
+})(jQuery);
